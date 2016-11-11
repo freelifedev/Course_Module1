c13a72c28213c674b3b29d56533e9615506a5ed2
sunggu <kisatsg@gmail.com>
2016-11-11 14:17:00 +0900

    initial commit with pycharm

---
+++ getting_and_setting.py
@@ -0,0 +1,61 @@
+# import the necessary packages
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# load the image, grab its dimensions, and show it
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+# cv2.imshow("Original", image)
+# cv2.waitKey()
+
+# images are just Numpyarrays. The top-left pixel can be found at (,0)
+(b, g, r) = image[225,111]
+print "Pixel at (0,0) - Red: {r}, Green: {g}, Blue: {b}".format(r=r, g=g, b=b)
+
+# now , let's changee the value of the pixel at (0,0) and make it red
+image[0,0] = (0,0,255)
+(b,g,r) = image[111,225]
+print "Pixel at (111,225) - Red: {r}, Green: {g}, Blue: {b}".format(r=r, g=g, b=b)
+
+cv2.imshow("Original", image)
+# cv2.waitKey()
+
+# compute the center of the image , which is simply the width and height
+# divided by two
+(cX, cY) = (w / 2, h/2 )
+
+# since we are using NumPy arrays, we can apply slicing and grab large chuks
+# of the imag -- let's grab the top-left corner
+tl = image[0:cY, 0:cX]
+cv2.imshow("Top-Left Cornet", tl)
+
+
+# in a similar fashion, let's grab the top-right, bottom-right, and bottom-left
+# corners and display them
+tr = image[0:cY, cX:w]
+br = image[cY:h, cX:w]
+bl = image[cY:h, 0:cX]
+# cv2.imshow("Top-Right Cornet" , tr)
+# cv2.imshow("Bottom-Right Corner", br)
+# cv2.imshow("Bottom-Left Corner", bl)
+cq = image[200:250, 100:150]
+
+
+# now let's make the top-left corner of the original image green
+# image[0:cY, 0:cX] = (0, 255, 0)
+
+# show our updated image
+cv2.imshow("Updated", image)
+# cv2.waitKey(0)
+
+
+print "{} pixels".format(383*972)
+
+
+
+
